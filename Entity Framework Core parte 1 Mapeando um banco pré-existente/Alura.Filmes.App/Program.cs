db5b680c351301120b89b69648e08fed663e7f7e
jenefferf <jeneffer.f@gmail.com>
2021-01-11 21:08:58 -0300

    Encaminhando códigos do curso

---
+++ Entity Framework Core parte 1 Mapeando um banco pré-existente/Alura.Filmes.App/Program.cs
@@ -0,0 +1,55 @@
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+             
+
+                //listar os 10 atores modificados recentemente 
+                var atores = contexto.Atores
+                    .OrderByDescending(a => EF.Property<DateTime>(a, "last_update"))
+                    .Take(10);
+
+                foreach (var ator in atores)
+                {
+                    Console.WriteLine(ator + " - " + contexto.Entry(ator).Property("last_update").CurrentValue);
+                }
+
+                /*
+                var ator = new Ator();
+                ator.PrimeiroNome = "Angelina";
+                ator.UltimoNome = "Jolie";
+                contexto.Entry(ator).Property("last_update").CurrentValue = DateTime.Now;
+
+
+                contexto.Atores.Add(ator);
+
+                contexto.SaveChanges(); */
+
+            }
+
+            //using (var contexto = new AluraFilmesContexto())
+            //{
+            //    contexto.LogSQLToConsole();
+
+            //    foreach (var ator in contexto.Atores)
+            //    {
+            //        System.Console.WriteLine(ator);
+            //    }
+            //}
+        }
+    }
+}
